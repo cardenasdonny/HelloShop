9b8e4e6466d8b6a4d1a52738531a5eea5c3d7f29
Dony <cardenasdonny@gmail.com>
2021-09-14 12:00:05 -0500

    Clase 17 HelloShop 10 Dtos identity parte 2

--- HelloShop/Controllers/UsuariosController.cs
+++ HelloShop/Controllers/UsuariosController.cs
@@ -1,6 +1,8 @@
 ﻿using HelloShop.Business.Abstract;
 using HelloShop.Business.Dtos.Usuarios;
 using HelloShop.Models.Entities;
+using HelloShop.WEB.Helpers;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -12,10 +14,12 @@ namespace HelloShop.WEB.Controllers
     public class UsuariosController : Controller
     {
         private readonly IUsuarioBusiness _usuarioBusiness;
+        private readonly SignInManager<Usuario> _signInManager;
 
-        public UsuariosController(IUsuarioBusiness usuarioBusiness)
+        public UsuariosController(IUsuarioBusiness usuarioBusiness, SignInManager<Usuario> signInManager)
         {
             _usuarioBusiness = usuarioBusiness;
+            _signInManager = signInManager;
         }
         public IActionResult Index()
         {
@@ -40,17 +44,46 @@ namespace HelloShop.WEB.Controllers
                     try
                     {
                         var usuarioId = await _usuarioBusiness.Crear(registrarUsuario);
+
+                        if (usuarioId == null)
+                            return Json(new { isValid = false, tipoError = "danger", error = "Error al crear el usuario" });
+
+                        if (usuarioId.Equals("ErrorPassword"))
+                            return Json(new { isValid = false, tipoError = "danger", error = "Error de password" });
+                        
+                        return Json(new { isValid = true, operacion = "crear" });
                     }
                     catch (Exception)
                     {
 
-                        throw;
+                        return Json(new { isValid = false, tipoError = "danger", error = "Error al crear el usuario" });
                     }
                 }
             }
+            return Json(new { isValid = false, tipoError = "warning", error = "Debe diligenciar los campos requeridos", html = Helper.RenderRazorViewToString(this, "Crear", registrarUsuario) });
 
 
-            return View(registrarUsuario);
+            
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDto loginDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var resultado = await _signInManager.PasswordSignInAsync(loginDto.Email, loginDto.Password, loginDto.RecordarMe, false);
+                if (resultado.Succeeded)
+                    return RedirectToAction("Dashboard","Admin");
+
+
+            }
+
+
+            return View();
         }
     }
 }
