faa9608594d5e67aaf625abfc5ce74b4b3e284ca
Dony <cardenasdonny@gmail.com>
2021-09-28 11:03:04 -0500

    Clase 21 HelloShop 14  API parte 1

---
+++ HelloShop.API/Controllers/ClientesController.cs
@@ -0,0 +1,37 @@
+﻿using HelloShop.Business.Abstract;
+using HelloShop.Business.Dtos.Clientes;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly IClienteBusiness _clienteBusiness;
+
+        public ClientesController(IClienteBusiness clienteBusiness)
+        {
+            _clienteBusiness = clienteBusiness;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<ClienteDetalleGestionarDto>> ObtenerClientes()
+        {
+            try
+            {
+                var clientes = await _clienteBusiness.ObtenerClientes();
+                return clientes;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
